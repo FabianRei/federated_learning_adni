b4aef587181db23542d77c96fa885472fda3890d
Fabianrei <gFriessm20*>
2019-07-25 23:54:30 +0200

    analysis of processed data

--- label_analysis.py
+++ label_analysis.py
@@ -2,11 +2,15 @@ import pickle
 from matplotlib import pyplot as plt
 import numpy as np
 import os
+import pandas as pd
 
-label_path = '/share/wandell/data/reith/federated_learning/labels_detailled2.pickle'
-label_path = r'C:\Users\Fabian\stanford\fed_learning\rsync\labels_detailled2.pickle'
+# label_path = '/share/wandell/data/reith/federated_learning/labels_detailled.pickle'
+label_path = r'C:\Users\Fabian\stanford\fed_learning\rsync\labels_detailled.pickle'
 with open(label_path, 'rb') as f:
     labels = pickle.load(f)
+excel_path = r'C:\Users\Fabian\stanford\fed_learning\rsync\Scanner information.gz.xls'
+with open(excel_path, 'rb') as f:
+    scan_sheet = pd.read_excel(f)
 
 scanners = []
 amyloid = []
@@ -14,6 +18,10 @@ rcf = []
 img_ids = []
 names = []
 slices = []
+site = []
+scan_keys = np.array(scan_sheet['Scanner'])
+scanner_keys = np.array([','.join(f.split(',')[:-1]) for f in scan_keys])
+scan_numbers = np.array(scan_sheet['Type'])
 for k, v in labels.items():
     scanners.append(f"{v['manufacturer']}, {v['model']}")
     amyloid.append(v['label'])
@@ -21,14 +29,22 @@ for k, v in labels.items():
     img_ids.append(v['img_id'])
     names.append(k)
     slices.append(v['slices'])
+    site.append(v['site'])
 
+site = np.array(site)
 names = np.array(names)
 slices = np.array(slices)
 img_ids = np.array(img_ids)
 scanners = np.array(scanners)
 amyloid = np.array(amyloid)
 rcf = np.array(rcf)
+types = []
+for s in scanners:
+    t = scan_numbers[scanner_keys == s]
+    types.append(int(t))
 
+for n, t in zip(names, types):
+    labels[n]['scanner_type'] = t
 fig = plt.figure()
 plt.title('scanners used in data')
 plt.hist(scanners)
