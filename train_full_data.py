b5bbb620adb5b6edd9fe8d530194247ec70e3815
Fabianrei <gFriessm20*>
2019-07-30 18:29:59 +0200

    normalize data

--- train_full_data.py
+++ train_full_data.py
@@ -19,6 +19,7 @@ if windows_db:
     h5_path = r'C:\Users\Fabian\stanford\fed_learning\federated_learning_data\slice_data.h5'
 else:
     h5_path = '/scratch/reith/fl/experiments/one_slice_dataset/slice_data.h5'
+extra_info = '_noPretrain_normalizeData'
 num_epochs = 30
 lr = 0.01
 decrease_after = 10
@@ -35,6 +36,10 @@ np.random.seed(42)
 shuff_idxs = np.random.permutation(len(data))
 data = data[shuff_idxs]
 labels = labels[shuff_idxs]
+# normalize data
+data -= data.mean()
+data /= data.std()
+
 if windows_db:
     data = data[:100]
     labels = labels[:100]
@@ -55,9 +60,9 @@ Net.cuda()
 criterion = nn.NLLLoss()
 out_path = os.path.dirname(h5_path)
 time_stamp = datetime.now().strftime('%m-%d_%H:%M')
-log_path = os.path.join(out_path, f"training_log_{time_stamp}.txt")
+log_path = os.path.join(out_path, f"training_log_{time_stamp}{extra_info}.txt")
 sys.stdout = Logger(log_path)
-csv_path = os.path.join(out_path, f"train_test_accuracy_{time_stamp}.csv")
+csv_path = os.path.join(out_path, f"train_test_accuracy_{time_stamp}{extra_info}.csv")
 csv_writer = CsvWriter(csv_path, header=['test_acc', 'train_acc', 'train_loss', 'epoch'])
 
 for i in range(num_epochs):
@@ -72,7 +77,7 @@ for i in range(num_epochs):
     Net, test_acc, test_pred_label, train_acc, train_loss, train_pred_label = train_result
     csv_writer.write_row(test_acc=test_acc, train_acc=train_acc, train_loss=train_loss, epoch=i)
     if save_pred_labels:
-        pickle_fn = os.path.join(out_path, f"epoch_{i}_pred_labels_train_test_epoch_{time_stamp}")
+        pickle_fn = os.path.join(out_path, f"epoch_{i}_pred_labels_train_test_epoch_{time_stamp}{extra_info}")
         pickle_object = {'train': train_pred_label, 'test': test_pred_label}
         with open(pickle_fn, 'wb') as f:
             pickle.dump(pickle_object, f)
