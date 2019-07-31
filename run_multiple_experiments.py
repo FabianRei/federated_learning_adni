3ce97c7bb4449511e10c8240cdfc3f1211434947
Fabianrei <gFriessm20*>
2019-07-31 13:58:10 +0200

    run multiple jobs

---
+++ run_multiple_experiments.py
@@ -0,0 +1,61 @@
+import os, sys, inspect
+# this is done to run things from console
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from dl.neural_network.train_h5 import train_h5
+import GPUtil
+import multiprocessing as mp
+import time
+import datetime
+
+
+def job_generator(jobs):
+    for job in jobs:
+        yield job
+
+
+def run_jobs(jobs):
+    device_ids = GPUtil.getAvailable(order='first', limit=6, maxLoad=0.1, maxMemory=0.1, excludeID=[], excludeUUID=[])
+    print(device_ids)
+    job_gen = job_generator(jobs)
+    process_dict = {}
+    while True:
+        try:
+            if process_dict == {}:
+                for device in device_ids:
+                    job = next(job_gen)
+                    print(f"Running {job} on GPU {device}")
+                    sub_proc = mp.Process(target=train_h5, args=[job[0]], kwargs=job[1])
+                    process_dict[str(device)] = sub_proc
+                    sub_proc.start()
+            for device, proc in process_dict.items():
+                if not proc.is_alive():
+                    job = next(job_gen)
+                    print(f"Running {job} on GPU {device}")
+                    sub_proc = mp.Process(target=train_h5, args=[job[0]], kwargs=job[1])
+                    process_dict[str(device)] = sub_proc
+                    sub_proc.start()
+        except StopIteration:
+            break
+
+        time.sleep(5)
+
+    for proc in process_dict.values():
+        proc.join()
+
+
+if __name__ == '__main__':
+    full_start = time.time()
+    h5_file = '/share/wandell/data/reith/federated_learning/data/one_slice_dataset/slice_data.h5'
+    jobs = [{'extra_info': 'pretrained', 'pretrained': True},
+            {'extra_info': 'non_pretrained', 'pretrained': False},
+            {'extra_info': 'pretrained_50epochs', 'pretrained': True, 'epochs': 50},
+            {'extra_info': 'non_pretrained_50epochs', 'pretrained': False, 'epochs': 50},
+            {'extra_info': 'pretrained_0_33_decrease', 'pretrained': True, 'decrease_after': 3, 'rate_of_decrease': 0.33},
+            {'extra_info': 'non_pretrained_0_33_decrease', 'pretrained': False, 'decrease_after': 3, 'rate_of_decrease': 0.33}]
+
+    jobs = [(h5_file, job) for job in jobs]
+    run_jobs(jobs)
+    print(f"Whole program finished! It took {str(datetime.timedelta(seconds=time.time()-full_start))} hours:min:seconds")
