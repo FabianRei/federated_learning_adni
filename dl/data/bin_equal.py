e7ad56ff7f349a2bf99acc963fd7482790768947
Fabianrei <gFriessm20*>
2019-09-01 13:44:42 +0200

    fix breaking point calc

--- dl/data/bin_equal.py
+++ dl/data/bin_equal.py
@@ -18,5 +18,6 @@ def bin_equal(arr, num_bins, middle=1.11):
         result.extend(space)
     result = np.array(result)
     result = result[unsort_idxs]
-    break_offs = [arr[result == i].max() for i in range(num_bins)]
+    unsort_arr = arr[unsort_idxs]
+    break_offs = [unsort_arr[result == i].max() for i in range(num_bins)]
     return result,  break_offs
