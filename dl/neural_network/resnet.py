e8af3eb1ca598e4918e4a5052d48613826bfc814
Fabianrei <gFriessm20*>
2019-09-20 18:46:02 +0200

    run more experiments

--- dl/neural_network/resnet.py
+++ dl/neural_network/resnet.py
@@ -3,6 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 import torch
 from fnmatch import fnmatch
+import math
 
 
 class ResNet50(nn.Module):
@@ -11,6 +12,7 @@ class ResNet50(nn.Module):
         self.channel_mean = torch.Tensor([0.485, 0.456, 0.406]).cuda().reshape(1, -1, 1, 1)
         self.channel_std = torch.Tensor([0.229, 0.224, 0.225]).cuda().reshape(1, -1, 1, 1)
         self.ResNet = models.resnet50(pretrained=pretrained)
+        self.pretrained = pretrained
         # pytorch's standard implementation throws errors at some image sizes..
         self.ResNet.avgpool = nn.AdaptiveAvgPool2d(1)
         self.ResNet.fc = nn.Linear(in_features=2048, out_features=num_classes, bias=True)
@@ -28,8 +30,9 @@ class ResNet50(nn.Module):
         if x.shape[1] == 1:
             x = x.repeat(1, 3, 1, 1)
         # substract imagenet mean and scale imagenet std
-        x -= self.channel_mean
-        x /= self.channel_std
+        if self.pretrained:
+            x -= self.channel_mean
+            x /= self.channel_std
         x = self.ResNet(x)
         return F.log_softmax(x, dim=1)
 
@@ -44,12 +47,31 @@ class ResNet50(nn.Module):
         for param in self.parameters():
             param.requires_grad = True
 
+
 class ResNet50Reg(nn.Module):
-    def __init__(self, pretrained=True, num_classes=1):
+    def __init__(self, pretrained=True, num_classes=1, num_input=3):
         super().__init__()
         self.channel_mean = torch.Tensor([0.485, 0.456, 0.406]).cuda().reshape(1, -1, 1, 1)
         self.channel_std = torch.Tensor([0.229, 0.224, 0.225]).cuda().reshape(1, -1, 1, 1)
         self.ResNet = models.resnet50(pretrained=pretrained)
+        self.pretrained = pretrained
+        # we assume multiples of 3
+        self.num_input = num_input
+        if num_input > 3 and not pretrained:
+            # experiment with higher input channels
+            new_input = nn.Conv2d(9, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            # standard resnet initialization for conv2d
+            n = new_input.kernel_size[0] * new_input.kernel_size[1] * new_input.out_channels
+            new_input.weight.data.normal_(0, math.sqrt(2. / n))
+            self.ResNet.conv1 = new_input
+        elif num_input > 3 and pretrained:
+            input_weights = self.ResNet.conv1.weight.data
+            # we divide by three, as this would yield the same activations, given the input slice was simply being
+            # copied
+            input_weights = input_weights.repeat(1,3,1,1)/3
+            new_input = nn.Conv2d(9, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            new_input.weight.data = input_weights
+            self.ResNet.conv1 = new_input
         # pytorch's standard implementation throws errors at some image sizes..
         self.ResNet.avgpool = nn.AdaptiveAvgPool2d(1)
         self.ResNet.fc = nn.Linear(in_features=2048, out_features=num_classes, bias=True)
@@ -67,8 +89,9 @@ class ResNet50Reg(nn.Module):
         if x.shape[1] == 1:
             x = x.repeat(1, 3, 1, 1)
         # substract imagenet mean and scale imagenet std
-        x -= self.channel_mean
-        x /= self.channel_std
+        if self.pretrained:
+            x -= self.channel_mean
+            x /= self.channel_std
         x = self.ResNet(x)
         return x  # F.log_softmax(x, dim=1)
 
@@ -83,3 +106,6 @@ class ResNet50Reg(nn.Module):
         for param in self.parameters():
             param.requires_grad = True
 
+
+if __name__ == '__main__':
+    net = ResNet50Reg(pretrained=True, num_input=9)
