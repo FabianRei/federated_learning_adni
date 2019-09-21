f88a3e1a57210261cf21cec9fd439c8bdf9fffa1
Fabianrei <gFriessm20*>
2019-09-21 15:11:55 +0200

    run more experiments

--- run_multiple_experiments.py
+++ run_multiple_experiments.py
@@ -57,8 +57,8 @@ if __name__ == '__main__':
     for sub in sub_folders:
         seed = int(sub.split('_')[-1])
         jobs = [
-            {'extra_info': '', 'pretrained': True, 'label_names': ['label_suvr', 'label_amyloid'], 'regression': True, 'lr': 0.001, 'seed': seed},
-            {'extra_info': '', 'pretrained': False, 'label_names': ['label_suvr', 'label_amyloid'], 'regression': True, 'lr': 0.001, 'seed': seed}]
+            {'extra_info': '', 'pretrained': True, 'label_names': ['label_suvr', 'label_amyloid'], 'regression': True, 'lr': 0.0001, 'seed': seed},
+            {'extra_info': '', 'pretrained': False, 'label_names': ['label_suvr', 'label_amyloid'], 'regression': True, 'lr': 0.0001, 'seed': seed}]
         h5_files = glob(f'{sub}/*_*/*.h5')
         # import pdb; pdb.set_trace()
         print(h5_files)
--- visualize/viz_training_progress.py
+++ visualize/viz_training_progress.py
@@ -10,7 +10,7 @@ import csv
 
 
 def get_csv_column(csv_path, col_name, sort_by=None, exclude_from=None):
-    df = pd.read_csv(csv_path, delimiter=';')
+    df = pd.read_csv(csv_path, delimiter=',')
     col = df[col_name].tolist()
     col = np.array(col)
     if sort_by is not None:
@@ -47,6 +47,47 @@ def viz_training(folder, identifier='', sort_by='epoch', title='default', train_
 
     plt.legend(frameon=True, loc='upper left', fontsize='small')
     fig.savefig(os.path.join(folder, f'{fname}.png'), dpi=200)
+    # fig.show()
+    print('done!')
+
+
+def viz_training_from_csv(csv_path, train_acc, test_acc, include_loss, train_loss='', test_loss=''):
+    csv_name = os.path.basename(csv_path).split('.')[0]
+    sort_by='epochs'
+    fname = f"viz_of_{csv_name}_loss_{include_loss}.png"
+    out_name = os.path.join(os.path.dirname(csv_path), fname)
+    fig, ax1 = plt.subplots()
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    colors = prop_cycle.by_key()['color']
+    ax1.yaxis.tick_right()
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Accuracy')
+    ax1.set_ylim([0.5, 1.05])
+    title = f"Training progression for {csv_name}"
+    # plt.title(title)
+    plt.grid(which='both')
+    train_acc = get_csv_column(csv_path, train_acc, sort_by=sort_by)
+    test_acc = get_csv_column(csv_path, test_acc, sort_by=sort_by)
+    epochs = get_csv_column(csv_path, 'epochs', sort_by=sort_by)
+    ax1.plot(epochs, train_acc, label='Train data accuracy', color=colors[0])
+    ax1.plot(epochs, test_acc, label='Test data accuracy', color=colors[1])
+    # loss = get_csv_column(csv_path, train_loss, sort_by=sort_by)
+    # ax1.plot(epochs, loss, label='Test data loss', color=colors[3])
+    if include_loss:
+        ax2 = ax1.twinx()
+        if fnmatch(csv_name, '*reg*'):
+            ax2.set_ylabel('Loss (MSE)')
+            ax2.set_ylim([-0.005, 1])
+        else:
+            ax2.set_ylabel('Loss (NLL)')
+        loss = get_csv_column(csv_path, train_loss, sort_by=sort_by)
+        ax2.plot(epochs, loss, label='Train data loss', color=colors[2])
+        if test_loss != '':
+            test_loss = get_csv_column(csv_path, test_loss, sort_by=sort_by)
+            ax2.plot(epochs, test_loss, label='Test data loss', color=colors[3])
+    # plt.legend(frameon=True, loc='upper left', fontsize='small')
+    # plt.tight_layout()
+    fig.savefig(out_name, dpi=200)
     # fig.show()
     print('done!')
 
@@ -97,109 +138,29 @@ def write_csv_row(resultCSV, testAcc, accOptimal, d1, d2, dataContrast, nn_dprim
                          'nn_dprime': nn_dprime})
 
 
-def get_spec_mean_std(csv_files, col_name, sort_by='epoch', include_raw=True):
-    res_accs = []
-    for f in csv_files:
-        res_acc = get_csv_column(f, col_name, sort_by=sort_by)
-        res_accs.append(res_acc)
-    res_accs = np.stack(res_accs)
-    mean_res = np.mean(res_accs, axis=0)
-    std_res = np.std(res_accs, axis=0)
-    if include_raw:
-        return mean_res, std_res, res_accs
-    else:
-        return mean_res, std_res
-
-
-def get_mean_std(csv_files, cols=['train_acc', 'test_acc'], sort_by='epoch'):
-    # assumes the same epochs for all csv files
-    epochs = get_csv_column(csv_files[0], 'epoch', sort_by=sort_by)+1
-    result_dict = {'epochs': epochs}
-    raw_dict = {}
-    for col in cols:
-        mean_res, std_res, raw_res = get_spec_mean_std(csv_files, col)
-        result_dict['mean_' + col] = mean_res
-        result_dict['std_' + col] = std_res
-        raw_dict[col] = raw_res
-    return result_dict, raw_dict
-
-
-def write_args2csv(dict, id, out_path):
-    csv_path = os.path.join(out_path, f'{id}.csv')
-    file_exists = os.path.isfile(csv_path)
-    if file_exists:
-        os.remove(csv_path)
-    headers = [h for h in dict.keys()]
-    with open(csv_path, 'a') as csvfile:
-        writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n', fieldnames=headers)
-        writer.writeheader()
-        for i in range(len(dict[headers[0]])):
-            row = {k: v[i] for k, v in dict.items()}
-            writer.writerow(row)
-
-
-def write_raw2csv(dict, id, out_path):
-    headers = list(range(10, 15))
-    for k, v in dict.items():
-        csv_path = os.path.join(out_path, f'{id}_{k}_raw.csv')
-        file_exists = os.path.isfile(csv_path)
-        if file_exists:
-            os.remove(csv_path)
-        with open(csv_path, 'a') as csvfile:
-            writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n', fieldnames=headers)
-            writer.writeheader()
-            for i in range(len(v[0])):
-                row = {k: val[i] for k, val in zip(headers, v)}
-                writer.writerow(row)
-
 if __name__ == '__main__':
-    super_path = r'C:\Users\Fabian\stanford\fed_learning\rsync\fl\experiments\seeds_more_dists'
-    fpaths = glob(os.path.join(super_path, '*seed*'))
-    dist_10 = glob(rf'{super_path}\*seed*\dist_10\*.csv')
-    one_slice = glob(rf'{super_path}\*seed*\one_slice\*.csv')
-    three_slice = glob(rf'{super_path}\*seed*\three_slice\*.csv')
-    csv_dict = {'dist_10': dist_10, 'one_slice': one_slice, 'three_slice': three_slice}
-
-    for key, val in csv_dict.items():
-        out_path = os.path.join(super_path, 'summary', key)
-        os.makedirs(out_path, exist_ok=True)
-        # case binary pretrained
-        case_id = 'binary_pretrained'
-        csv_files = filter(val, '*1_pretrained_3*')
-        columns = ['test_acc', 'train_acc', 'train_loss', 'test_label_acc_train', 'test_label_acc_test']
-        res, raw_res = get_mean_std(csv_files, columns)
-        viz_training_data(out_path, res['mean_train_acc'], res['mean_test_acc'], res['epochs'], identifier=case_id)
-        write_args2csv(res, case_id, out_path)
-        write_raw2csv(raw_res, case_id, out_path)
-        # case binary non pretrained
-        case_id = 'binary_non_pretrained'
-        csv_files = filter(val, '*non_pretrained_3*')
-        columns = ['test_acc', 'train_acc', 'train_loss', 'test_label_acc_train', 'test_label_acc_test']
-        res, raw_res = get_mean_std(csv_files, columns)
-        viz_training_data(out_path, res['mean_train_acc'], res['mean_test_acc'], res['epochs'], identifier=case_id)
-        write_args2csv(res, case_id, out_path)
-        write_raw2csv(raw_res, case_id, out_path)
-        # case pretrained regression
-        case_id = 'pretrained_regression'
-        csv_files = filter(val, '*1_pretrained_reg*')
-        columns = ['test_acc', 'train_acc', 'train_loss', 'test_label_acc_train', 'test_label_acc_test']
-        res, raw_res = get_mean_std(csv_files, columns)
-        viz_training_data(out_path, res['mean_test_label_acc_train'], res['mean_test_label_acc_test'], res['epochs'], identifier=case_id)
-        write_args2csv(res, case_id, out_path)
-        write_raw2csv(raw_res, case_id, out_path)
-        # case non retrained regression
-        case_id = 'non_pretrained_regression'
-        csv_files = filter(val, '*non_pretrained_reg*')
-        columns = ['test_acc', 'train_acc', 'train_loss', 'test_label_acc_train', 'test_label_acc_test']
-        res, raw_res = get_mean_std(csv_files, columns)
-        viz_training_data(out_path, res['mean_test_label_acc_train'], res['mean_test_label_acc_test'], res['epochs'], identifier=case_id)
-        write_args2csv(res, case_id, out_path)
-        write_raw2csv(raw_res, case_id, out_path)
+    super_path = r'C:\Users\Fabian\stanford\fed_learning\rsync\fl\experiments\seeds\summary'
+    fpaths = glob(os.path.join(super_path, '*_*'))
+    for fpath in fpaths:
+        # binary case
+        csvs = glob(os.path.join(fpath, '*binary_*pretrained.csv'))
+        for c in csvs:
+            viz_training_from_csv(c, 'mean_train_acc', 'mean_test_acc', include_loss=True, train_loss='mean_train_loss')
+        csvs = glob(os.path.join(fpath, '*pretrained_regression.csv'))
+        for c in csvs:
+            viz_training_from_csv(c, 'mean_test_label_acc_train', 'mean_test_label_acc_test', include_loss=True, train_loss='mean_train_acc', test_loss='mean_test_acc')
+    print("nice")
 
 
 
 
 
+
+
+
+
+r'''
+##############PAST RUNS###################################
 
     for f in fpaths:
         folder = glob(os.path.join(f, '*_*'))
@@ -215,11 +176,7 @@ if __name__ == '__main__':
             for ident in ids_bin:
                 viz_training(fpath, ident, train_acc='test_label_acc_train', test_acc='test_label_acc_test',
                              fname_addition='_bin_test')
-
-
-
-r'''
-##############PAST RUNS###################################
+#####################################################
 if __name__ == '__main__':
     super_path = r'C:\Users\Fabian\stanford\fed_learning\rsync\fl\experiments\seeds'
     fpaths = glob(os.path.join(super_path, '*seed*'))
