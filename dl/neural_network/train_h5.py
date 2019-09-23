c09218180826f3daf266feb27d06058063b4e256
Fabianrei <gFriessm20*>
2019-09-23 18:26:43 +0200

    run more experiments

--- dl/neural_network/train_h5.py
+++ dl/neural_network/train_h5.py
@@ -248,6 +248,7 @@ if __name__ == '__main__':
     current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     parent_dir = os.path.dirname(current_dir)
     parent_dir = os.path.dirname(parent_dir)
+    print(parent_dir)
     sys.path.insert(0, parent_dir)
     windows_db = False
     if not windows_db:
