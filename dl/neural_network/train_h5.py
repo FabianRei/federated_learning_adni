d7385650e637a07e9c4d43d25a9dae7a8596ddf3
Fabianrei <gFriessm20*>
2019-09-25 12:04:35 +0200

    run more experiments

--- dl/neural_network/train_h5.py
+++ dl/neural_network/train_h5.py
@@ -33,7 +33,7 @@ def train_h5(h5_path, num_epochs=30, label_names=None, extra_info='', lr=0.01, d
              rate_of_decrease=0.1, gpu_device=-1, save_pred_labels=True, test_split=0.2, pretrained=True,
              batch_size=32, binning=-1, regression=False, include_subject_ids=True, seed=-1, freeze_epochs=-1,
              use_resnext=False, save_model=True):
-    windows_db = True
+    windows_db = False
     if windows_db:
         h5_path = r'C:\Users\Fabian\stanford\fed_learning\federated_learning_data\dist_20_incl_subjects_site_three_slices_dataset\slice_data_subj.h5'
 
