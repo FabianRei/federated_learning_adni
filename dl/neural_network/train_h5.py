b095bee370643d7f21014fcabfd1fb6cd555650e
Fabianrei <gFriessm20*>
2019-10-07 17:53:25 +0200

    run more experiments

--- dl/neural_network/train_h5.py
+++ dl/neural_network/train_h5.py
@@ -203,7 +203,7 @@ def train_h5(h5_path, num_epochs=30, label_names=None, extra_info='', lr=0.01, d
         if use_resnext:
             Net = ResNext101(pretrained=pretrained, num_classes=num_classes, num_input=num_chan_input)
         elif use_resnet152:
-            Net = ResNet152(pretrained=pretrained, num_classes=1, num_input=num_chan_input)
+            Net = ResNet152(pretrained=pretrained, num_classes=num_classes, num_input=num_chan_input)
         else:
             Net = ResNet50(pretrained=pretrained, num_classes=num_classes, num_input=num_chan_input)
         criterion = nn.NLLLoss()
